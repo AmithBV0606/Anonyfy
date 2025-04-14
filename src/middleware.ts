07c3b7b1bf922e67796b0ead18c1cb4590aef1ed
Amith B V <amithrao0606@gmail.com>
2025-04-14 08:44:13 +0530

    Created middlewares.

---
+++ src/middleware.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// To read JSON Web Token from an API route
+import { getToken } from "next-auth/jwt";
+
+// Next-Auth
+export { default } from "next-auth/middleware";
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+  const token = await getToken({ req: request });
+  const url = request.nextUrl;
+
+  if (
+    token &&
+    (url.pathname.startsWith("/sign-in") ||
+      url.pathname.startsWith("/sign-up") ||
+      url.pathname.startsWith("/verify") ||
+      url.pathname === "/")
+  ) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  if (!token && url.pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/home", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/sign-in", "/sign-up", "/", "/verify/:path*", "/dashboard/:path*"],
+};
