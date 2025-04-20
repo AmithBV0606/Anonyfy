f0fe928b640ae28cce4e35f31abd81fd8dbccf7b
Amith B V <amithrao0606@gmail.com>
2025-04-20 13:23:39 +0530

    Sign-in page completed!

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,21 +1,126 @@
 "use client";
 
-import { useSession, signIn, signOut } from "next-auth/react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { signIn } from "next-auth/react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { signInSchema } from "@/schemas/signInSchema";
 
-export default function Component() {
-  const { data: session } = useSession();
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
+function page() {
+  const router = useRouter();
+
+  // Zod implementation
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      identifier: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+    console.log(data);
+    const result = await signIn("credentials", {
+      redirect: false,
+      identifier: data.identifier,
+      password: data.password,
+    });
+    console.log(result);
+
+    if (result?.error) {
+      if (result.error === "CredentialsSignin") {
+        toast("Incorrect username or password");
+      } else {
+        toast(result.error);
+      }
+    }
+
+    if (result?.url) {
+      router.replace("/dashboard");
+    }
+  };
+
   return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()} className="bg-black p-2 text-white rounded-lg cursor-pointer">Sign in</button>
-    </>
+    <div className="flex justify-center items-center min-h-screen bg-black inset-0 shadow-2xl shadow-amber-200">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Welcome Back to Anonyfy
+          </h1>
+          <p className="mb-4">Sign in to continue your secret conversations</p>
+        </div>
+
+        {/* Form */}
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            {/* Email field : */}
+            <FormField
+              control={form.control}
+              name="identifier"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email/Username</FormLabel>
+
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* Password field : */}
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+
+                  <FormControl>
+                    <Input type="password" placeholder="password" {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button type="submit" className="w-full">
+              Sign In
+            </Button>
+          </form>
+        </Form>
+
+        {/* Already a member? */}
+        <div className="text-center mt-4">
+          <p className="space-x-2">
+            <span>Don't have an account yet?</span>
+            <Link
+              href={"/sign-up"}
+              className="text-blue-600 hover:text-blue-800 underline"
+            >
+              Sign Up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
+
+export default page;
