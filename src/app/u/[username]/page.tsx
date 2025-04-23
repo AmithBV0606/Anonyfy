612b6989e2318ef9d5d33da24fa81f636c5a92e3
Amith B V <amithrao0606@gmail.com>
2025-04-23 13:25:31 +0530

    Added a feature where audience can send the message to the creator, using the creator's public profile link.

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -1,9 +1,116 @@
-import React from 'react'
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+import { messageSchema } from "@/schemas/messageSchema";
+import { ApiResponse } from "@/types/ApiResponse";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios, { AxiosError } from "axios";
+import { Loader2 } from "lucide-react";
+import { useParams } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import * as z from "zod";
 
 function page() {
+  const { username } = useParams<{ username: string }>();
+
+  // Loading state for submitting the message
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Zod implementation
+  const form = useForm<z.infer<typeof messageSchema>>({
+    resolver: zodResolver(messageSchema),
+    defaultValues: {
+      content: "",
+    },
+  });
+
+  // Function to call API which will save the message in the database
+  const onSubmit = async (data: z.infer<typeof messageSchema>) => {
+    setIsLoading(true);
+    // console.log(data);
+
+    try {
+      const response = await axios.post("/api/send-message", {
+        username,
+        content: data.content,
+      });
+
+      if (response) {
+        setIsLoading(false);
+        toast(response.data.message);
+      }
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiResponse>;
+      toast(
+        axiosError.response?.data.message ||
+          `Error while sending the message to ${username}!!`
+      );
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <div>Message page!!</div>
-  )
+    <div className="container mx-auto my-8 p-6 bg-white rounded max-w-4xl">
+      <h1 className="text-2xl md:text-4xl font-extrabold mb-12 text-center">
+        <span className="underline">{username.toLocaleUpperCase()}'s</span>{" "}
+        Public Profile
+      </h1>
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-full space-y-4"
+        >
+          <FormField
+            control={form.control}
+            name="content"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="font-bold">
+                  Send Anonymous Message to @{username}
+                </FormLabel>
+
+                <FormControl>
+                  <Textarea
+                    placeholder="Write your anonymous message here"
+                    className="resize-none"
+                    {...field}
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className="flex justify-start">
+            {isLoading ? (
+              <Button disabled>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                Please wait
+              </Button>
+            ) : (
+              <Button type="submit" disabled={isLoading}>
+                Send It
+              </Button>
+            )}
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
 }
 
-export default page
+export default page;
--- src/schemas/messageSchema.ts
+++ src/schemas/messageSchema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const contentValidation = z
   .string()
   .min(5, { message: "Content must be atleast 5 characters long!!" })
-  .max(300, { message: "Content must be no longer than 300 characters!!" });
+  .max(200, { message: "Content must be no longer than 200 characters!!" });
 
 export const messageSchema = z.object({
   content: contentValidation,
