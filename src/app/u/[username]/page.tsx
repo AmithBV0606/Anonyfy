fafc7948e0ee2ac067d4642186b899dd903014db
Amith B V <amithrao0606@gmail.com>
2025-04-23 20:09:12 +0530

    Created a feature where users can generate random questions by clicking the "suggest essages" button.

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import {
   Form,
   FormControl,
@@ -20,12 +21,33 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import * as z from "zod";
+import AllMessages from "@/All-Messages.json";
+import { QuestionProps } from "@/types/Questions";
 
 function page() {
   const { username } = useParams<{ username: string }>();
 
   // Loading state for submitting the message
   const [isLoading, setIsLoading] = useState(false);
+
+  // For loading top 3 questions
+  const [suggestedQuestions, setSuggestedQuestions] = useState<QuestionProps[]>(
+    [
+      {
+        id: 1,
+        question: "What’s a hobby you’ve recently started?",
+      },
+      {
+        id: 2,
+        question:
+          "If you could have dinner with any historical figure, who would it be?",
+      },
+      {
+        id: 3,
+        question: "What’s a simple thing that makes you happy?",
+      },
+    ]
+  );
 
   // Zod implementation
   const form = useForm<z.infer<typeof messageSchema>>({
@@ -55,6 +77,7 @@ function page() {
         toast(response.data.message);
       }
     } catch (error) {
+      form.reset();
       const axiosError = error as AxiosError<ApiResponse>;
       toast(
         axiosError.response?.data.message ||
@@ -63,6 +86,26 @@ function page() {
     } finally {
       setIsLoading(false);
     }
+  };
+
+  // Function to fetch the suggested messages
+  const fetchSuggestedMessages = async () => {
+    let newFilteredQuestion: QuestionProps[] = [];
+
+    for (let index = 0; index < 3; index++) {
+      const randomNum = Math.floor(Math.random() * 30);
+      // find : returns an object
+      // filter returns an array(Mistake I made)
+      const FilteredQuestions = AllMessages.find((message) => {
+        return message.id === randomNum;
+      });
+
+      if (FilteredQuestions) {
+        newFilteredQuestion.push(FilteredQuestions);
+      }
+    }
+
+    setSuggestedQuestions(newFilteredQuestion);
   };
 
   return (
@@ -101,18 +144,50 @@ function page() {
 
           <div className="flex justify-start">
             {isLoading ? (
-              <Button disabled>
+              <Button disabled className="cursor-pointer">
                 <Loader2 className="mr-2 h-4 w-4 animate-spin" />
                 Please wait
               </Button>
             ) : (
-              <Button type="submit" disabled={isLoading || !messageContent}>
+              <Button type="submit" disabled={isLoading || !messageContent} className="cursor-pointer">
                 Send It
               </Button>
             )}
           </div>
         </form>
       </Form>
+
+      {/* Button to suggest/generate new messages */}
+      <div className="my-8 space-y-4">
+        <div className="space-y-2">
+          <Button
+            variant={"default"}
+            className="my-4 cursor-pointer"
+            onClick={fetchSuggestedMessages}
+          >
+            Suggest Messages
+          </Button>
+
+          <p className="font-semibold">
+            Click on any message below to select it!
+          </p>
+        </div>
+      </div>
+
+      {/* List of messages */}
+      <Card>
+        <CardHeader>
+          <h3 className="text-xl font-semibold">Messages</h3>
+        </CardHeader>
+
+        <CardContent className="flex flex-col space-y-4">
+          {suggestedQuestions.map((suggestedQuestion, index) => (
+            <Button key={index} variant="outline" className="mb-2 font-semibold cursor-pointer">
+              {suggestedQuestion.question}
+            </Button>
+          ))}
+        </CardContent>
+      </Card>
     </div>
   );
 }
