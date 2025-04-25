a9cab4b2d02fedc845d2bcc85aaffe87f63c258e
Amith B V <amithrao0606@gmail.com>
2025-04-25 19:56:48 +0530

    Re-designed verification page.

--- src/app/(auth)/verify/[username]/page.tsx
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -10,16 +10,22 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
 import { verifySchema } from "@/schemas/verifySchema";
 import { ApiResponse } from "@/types/ApiResponse";
 import { zodResolver } from "@hookform/resolvers/zod";
 import axios, { AxiosError } from "axios";
+import { Shield } from "lucide-react";
+import Link from "next/link";
 import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import * as z from "zod";
 
 export default function page() {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
   const { username } = useParams<{ username: string }>();
 
   const router = useRouter();
@@ -33,56 +39,95 @@ export default function page() {
   });
 
   const onSubmit = async (data: z.infer<typeof verifySchema>) => {
+    setIsSubmitting(true);
+
     try {
       const response = await axios.post(`/api/verify-code`, {
         username,
         code: data.code,
       });
-      toast(response.data.message);
-      router.replace("/sign-in");
+
+      if (response) {
+        setIsSubmitting(false);
+        toast(response.data.message);
+        router.replace("/sign-in");
+      }
     } catch (error) {
       console.error("Error in verifying the code", error);
       const axiosError = error as AxiosError<ApiResponse>;
+      setIsSubmitting(false);
       let errorMessage = axiosError.response?.data.message;
       toast(errorMessage);
     }
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-[#0D0C1D]">
-      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
-        <div className="text-center">
-          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
-            Verify Your Account
-          </h1>
+    <div className="min-h-screen bg-gradient-to-br from-black via-gray-900 to-black text-gray-100 flex flex-col">
+      {/* Header/Navbar */}
+      <div className="flex items-center justify-between p-6">
+        <Link href="/" className="flex items-center gap-2">
+          <Shield className="h-6 w-6 text-gray-100" />
 
-          <p className="mb-4">
-            Enter the verification code sent to your email.
-          </p>
-        </div>
-
-        {/* Form */}
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-            {/* Verification code field : */}
-            <FormField
-              control={form.control}
-              name="code"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Verification Code</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Enter the code..." {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <Button type="submit">Verify</Button>
-          </form>
-        </Form>
+          <span className="text-2xl font-bold">Anonyfy</span>
+        </Link>
       </div>
+
+      {/* Separator */}
+      <Separator className="bg-gray-800 px-0" />
+
+      {/* Form component : */}
+      <div className="flex-1 flex items-center justify-center px-4 py-12">
+        <div className="w-full max-w-md">
+          <div className="bg-gradient-to-br from-gray-800 to-gray-900 p-8 rounded-xl border border-gray-700 shadow-xl">
+            {/* Main Heading : */}
+            <div className="text-center mb-8">
+              <h1 className="text-2xl font-bold">Verify your Account</h1>
+
+              <p className="text-gray-400 mt-2">
+                Enter the OTP Sent to Your Email Address.
+              </p>
+            </div>
+
+            {/* Actual Form : */}
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-6"
+              >
+                {/* Verification code field : */}
+                <FormField
+                  control={form.control}
+                  name="code"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="mb-2">Verification Code</FormLabel>
+
+                      <FormControl>
+                        <Input placeholder="Enter the Code" {...field} />
+                      </FormControl>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <Button
+                  type="submit"
+                  className="w-full mt-2 cursor-pointer py-4"
+                  variant={"secondary"}
+                >
+                  Verify
+                </Button>
+              </form>
+            </Form>
+          </div>
+        </div>
+      </div>
+
+      {/* New Footer : */}
+      <footer className="py-6 text-center text-gray-300 text-md">
+        <p>© {new Date().getFullYear()} Anonyfy. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
